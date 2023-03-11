922c86d0b366b6581f855102378159e2bae0da73
GrayStuido <113940755+GrayStuido@users.noreply.github.com>
2023-03-11 11:30:25 -0800

    Update Text Splash.js

--- Text Splash.js
+++ Text Splash.js
@@ -55,3 +55,9 @@
         "This Is A Hood Classic",
         "Neverita - Bad Bunny",
       ];
+
+      function changeText() {
+        const randomIndex = Math.floor(Math.random() * options.length);
+        const randomText = options[randomIndex];
+        document.getElementById("random-h1").textContent = randomText;
+      }
